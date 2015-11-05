d2fe8a199f73f89f90166d6a69c3320f55721ed7
Seba Gamboa <me@sagmor.com>
2015-11-04 21:10:30 -0300

    Improved docs. [ci skip]

--- lib/yaks/syro.rb
+++ lib/yaks/syro.rb
@@ -3,12 +3,21 @@ require 'yaks'
 
 require "yaks/syro/version"
 
-module Yaks
+module Yaks # :nodoc:
+
+  # Yaks integration for Syro
   module Syro
     def self.included(deck)
       deck.extend ClassMethods
     end
 
+    # Render a yaks from inside a Syro route.
+    #
+    # @params object The object you want to render
+    # @params opts [Hash] render options
+    # @option opts [Class] :mapper The mapper class to use
+    # @option opts [Symbol] :format The serialization format to use
+    #
     def yaks(object, opts = {})
       runner = self.class.yaks_config.runner(object, {env: env}.merge(opts))
 
@@ -17,8 +26,29 @@ module Yaks
     end
 
     module ClassMethods
+
+      # Yaks configuration object
+      #
+      # @return [Yaks]
       attr_reader :yaks_config
 
+      # Configure Yaks
+      #
+      # @example
+      #     class MyApp < Syro::Deck
+      #       include Yaks::Syro
+      #
+      #       configure_yaks do
+      #         default_format :hal
+      #         rel_template 'http://api.example.com/rels/{rel}'
+      #         format_options(:hal, plural_links: [:copyright])
+      #         mapper_namespace ::MyAPI
+      #         json_serializer do |data|
+      #           JSON.dump(data)
+      #         end
+      #       end
+      #     end
+      #
       def configure_yaks(&block)
         @yaks_config = ::Yaks.new(&block)
       end
